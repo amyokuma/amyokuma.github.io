d5febe7c5083c5844599654161fe6ad3f8582ef4
Amy Okuma <amyokuma@gmail.com>
2024-08-24 21:35:59 -0700

    add half circle svg to contact section

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -6,7 +6,7 @@ function Project({ number, imageSrc, subject, title, videoSrc}) {
     <div className="bg-transparent flex mb-12 gap-72">
         <h1 className="bg-transparent font-semibold text-[400px] text-[#F2F0E9] w-[400px] ml-[-1rem]">{number}.</h1>
         <div className="bg-transparent relative">
-          <video src={videoSrc} className="absolute h-auto w-[800px] rounded-xl top-[270px] left-[40px]" autoPlay muted loop />
+          <video src={videoSrc} className="absolute h-auto w-[800px] rounded-xl top-[15rem] left-[40px]" autoPlay muted loop />
           <img src={imageSrc} className="h-[1100px] w-[956px] rounded-2xl"/>
           <p className="bg-transparent text-[#F2F0E9] font-thin text-xl mt-4 font leading-4">{subject}</p>
           <p className="bg-transparent text-[#F2F0E9] font-bold text-5xl mt-2">{title}</p>
